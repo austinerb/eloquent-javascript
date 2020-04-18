3c8d2a154fc8f5edaaa885dc8873d0f72f9a33c4
Austin Erb <austin.erb8@gmail.com>
2020-04-17 23:47:19 -0400

    Init commit

---
+++ exercises.js
@@ -0,0 +1,12 @@
+// CHAPTER 2
+
+// Looping a triangle
+
+// for (let i = 1; i < 8; i++) {
+//     for (let j = 0; j < i; j++) {
+//         console.log("#");
+//     }
+//     console.log("\n");
+// }
+
+// FizzBuzz
