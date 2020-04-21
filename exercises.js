d8eabc108a3fa4bf0a890d2f29ea305129912ca0
Austin Erb <austin.erb8@gmail.com>
2020-04-21 01:44:39 -0400

    Finish Chapter 5 exercises

--- exercises.js
+++ exercises.js
@@ -320,3 +320,67 @@
 // console.log("obj", "obj", deepEqual(obj, obj));
 // console.log("obj", "non obj", deepEqual(obj, {here: 1, object: 2}));
 // console.log("obj", "eq obj", deepEqual(obj, {here: {is: "an"}, object: 2}));
+
+
+
+// CHAPTER 5
+
+
+// Flattening (DIDN'T SOLVE)
+
+// Use the reduce method in combination with the concat method to “flatten” an
+// array of arrays into a single array that has all the elements of the original
+// arrays.
+
+// let arrays = [[1, 2, 3], [4, 5], [6]];
+//
+// var flat = arrays.reduce(function(a, b) {
+//       return a.concat(b);
+// });
+//
+// console.log(flat);
+
+
+// Your Own Loop
+
+// Write a higher-order function loop that provides something like a for loop
+// statement. It takes a value, a test function, an update function, and a body
+// function. Each iteration, it first runs the test function on the current loop
+// value and stops if that returns false. Then it calls the body function,
+// giving it the current value. Finally, it calls the update function to create
+// a new value and starts from the beginning.
+
+// function loop(val, test, update, body) {
+//   while (test(val)) {
+//     body(val);
+//     val = update(val);
+//   }
+// }
+//
+// loop(3, n => n > 0, n => n - 1, console.log);
+
+
+// Everything
+
+// Implement every as a function that takes an array and a predicate function
+// as parameters. Write two versions, one using a loop and one using the some
+// method.
+
+// function every1(arr, test) {
+//   for (let i in arr) {
+//     if (!test(arr[i])) return false;
+//   }
+//   return true;
+// }
+//
+// console.log(every1([1, 3, 5], n => n < 10));
+// console.log(every1([2, 4, 16], n => n < 10));
+// console.log(every1([], n => n < 10));
+//
+// function every2(arr, test) {
+//   return !arr.some(e => !test(e));
+// }
+//
+// console.log(every2([1, 3, 5], n => n < 10));
+// console.log(every2([2, 4, 16], n => n < 10));
+// console.log(every2([], n => n < 10));
