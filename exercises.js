7897590380a9eea52840af78d468458169ccb455
Austin Erb <austin.erb8@gmail.com>
2020-04-22 22:39:07 -0400

    Start Chhapter 6

--- exercises.js
+++ exercises.js
@@ -384,3 +384,45 @@
 // console.log(every2([1, 3, 5], n => n < 10));
 // console.log(every2([2, 4, 16], n => n < 10));
 // console.log(every2([], n => n < 10));
+
+
+// CHAPTER 6
+
+// A Vector Type
+
+// Write a class Vec that represents a vector in two-dimensional space. It
+// takes x and y parameters (numbers), which it should save to properties of
+// the same name.
+
+// Give the Vec prototype two methods, plus and minus, that take another
+// vector as a parameter and return a new vector that has the sum or difference
+// of the two vectors’ (this and the parameter) x and y values.
+
+// Add a getter property length to the prototype that computes the length of
+// the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+
+// class Vec {
+//   constructor (x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//
+//   plus(plusVec) {
+//     return new Vec(this.x + plusVec.x, this.y + plusVec.y);
+//   }
+//
+//   minus(minusVec) {
+//     return new Vec(this.x - minusVec.x, this.y - minusVec.y);
+//   }
+//
+//   get length() {
+//     return Math.sqrt(this.x * this.x + this.y * this.y);
+//   }
+// }
+//
+// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// console.log(new Vec(3, 4).length);
+
+
+// Groups
